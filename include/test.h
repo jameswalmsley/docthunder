ca4ec771eaf4d686a0b4eda89015965c20a00c76
James Walmsley <james@fullfat-fs.co.uk>
2013-02-09 16:57:45 +0000

    Initial test header for developing the parser.

---
+++ include/test.h
@@ -0,0 +1,28 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+
+#define MAX_PATH 1024 		///< Defines the maximum path length
+#define 	MAX_PATH_LEN 		2048		///< Another max_path length
+
+
+
+/**
+ *	@brief	Program's main entry function.
+ *	
+ *	This is where all the magic starts.
+ *
+ *	@param	argc	The argument count.
+ *	@param	argv	The array of argument strings.
+ *
+ *	@return	An error code.
+ *
+ **/
+int main(int argc, char **argv);
+
+
+
+
+
+
+
+#endif
